9220d84fafcd9624c2f5f9ba6543f22f84179385
Johanpmeert <johan_meert@skynet.be>
2024-02-24 09:51:51 +0100

    added webcam support to create a random private key

--- src/com/johanpmeert/Main.java
+++ src/com/johanpmeert/Main.java
@@ -1,17 +1,21 @@
 package com.johanpmeert;
 
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.*;
 import java.awt.event.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.awt.print.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Hashtable;
 
+import com.github.sarxos.webcam.Webcam;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
@@ -31,8 +35,8 @@ public class Main implements Printable, ActionListener {
     private static GenerateKeys generatedKeys;
     public static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private enum Actions {
-        PRINT, REGENERATE, QUIT, COPYWIF, COPYBTC
+    public enum Actions {
+        PRINT, REGENERATE, SHOW_QR, QUIT, COPY_WIF, COPY_BTC, WEBCAM, PASTE_SEED
     }
 
     public static void main(String[] args) {
@@ -40,7 +44,7 @@ public class Main implements Printable, ActionListener {
         generatedKeys = new GenerateKeys();
         // Create UI
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-        JFrame f = new JFrame("Bitcoin paper wallet printer using SecureRandom");
+        JFrame f = new JFrame("Bitcoin paper wallet printer using SecureRandom or WebCAM picture");
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
@@ -49,30 +53,52 @@ public class Main implements Printable, ActionListener {
         f.setSize(750, 200);
         // Create buttons
         JButton printButton = new JButton("Print paper wallet");
-        printButton.setBounds(10, 120, 150, 30);
+        printButton.setBounds(10, 120, 135, 30);
         printButton.addActionListener(new Main());
         printButton.setActionCommand(Actions.PRINT.name());
         f.add(printButton);
         JButton regenerateButton = new JButton("Regenerate keys");
-        regenerateButton.setBounds(200, 120, 150, 30);
+        regenerateButton.setBounds(160, 120, 125, 30);
         regenerateButton.addActionListener(new Main());
         regenerateButton.setActionCommand(Actions.REGENERATE.name());
         f.add(regenerateButton);
+        JButton qrButton = new JButton("Show PublicKey QR");
+        qrButton.setBounds(300, 120, 150, 30);
+        qrButton.addActionListener(new Main());
+        qrButton.setActionCommand(Actions.SHOW_QR.name());
+        f.add(qrButton);
         JButton quitButton = new JButton("Quit");
-        quitButton.setBounds(400, 120, 100, 30);
+        quitButton.setBounds(600, 120, 100, 30);
         quitButton.addActionListener(new Main());
         quitButton.setActionCommand(Actions.QUIT.name());
         f.add(quitButton);
+        JButton pasteHexSeed = new JButton("PASTE");
+        pasteHexSeed.setBounds(625, 10, 75, 25);
+        pasteHexSeed.addActionListener(new Main());
+        pasteHexSeed.setActionCommand(Actions.PASTE_SEED.name());
+        f.add(pasteHexSeed);
         JButton copyWifButton = new JButton("COPY");
         copyWifButton.setBounds(625, 40, 75, 25);
         copyWifButton.addActionListener(new Main());
-        copyWifButton.setActionCommand(Actions.COPYWIF.name());
+        copyWifButton.setActionCommand(Actions.COPY_WIF.name());
         f.add(copyWifButton);
         JButton copyBtcButton = new JButton("COPY");
         copyBtcButton.setBounds(625, 70, 75, 25);
         copyBtcButton.addActionListener(new Main());
-        copyBtcButton.setActionCommand(Actions.COPYBTC.name());
+        copyBtcButton.setActionCommand(Actions.COPY_BTC.name());
         f.add(copyBtcButton);
+        // WebCam button
+        Webcam webcam = Webcam.getDefault();
+        JButton webcamButton = new JButton("WebCAM");
+        webcamButton.setBounds(465, 120, 100, 30);
+        webcamButton.addActionListener(new Main());
+        webcamButton.setActionCommand(Actions.WEBCAM.name());
+        f.add(webcamButton);
+        if (webcam == null) {
+            webcamButton.setEnabled(false);
+        } else {
+            webcam.close();
+        }
         // Create Text labels
         JLabel hexRandomLabel = new JLabel("Hex random seed:");
         hexRandomLabel.setBounds(10, 10, 100, 20);
@@ -86,11 +112,11 @@ public class Main implements Printable, ActionListener {
         WIFValueLabel = new JLabel(generatedKeys.privateKey);
         WIFValueLabel.setBounds(150, 40, 400, 20);
         f.add(WIFValueLabel);
-        JLabel bitcoinAddressLabel = new JLabel("Bitcoin address (Segwit):");
-        bitcoinAddressLabel.setBounds(10, 70, 140, 20);
+        JLabel bitcoinAddressLabel = new JLabel("Bitcoin address (Segwit P2SH):");
+        bitcoinAddressLabel.setBounds(10, 70, 170, 20);
         f.add(bitcoinAddressLabel);
         bitcoinAddressValueLabel = new JLabel(generatedKeys.bitcoinAddress);
-        bitcoinAddressValueLabel.setBounds(150, 70, 400, 20);
+        bitcoinAddressValueLabel.setBounds(180, 70, 400, 20);
         f.add(bitcoinAddressValueLabel);
         // Finalize layout
         f.setLayout(null);
@@ -114,14 +140,63 @@ public class Main implements Printable, ActionListener {
             updateLabels();
         } else if (e.getActionCommand().equals(Actions.QUIT.name())) {
             System.exit(0);
-        } else if (e.getActionCommand().equals(Actions.COPYWIF.name())) {
+        } else if (e.getActionCommand().equals(Actions.COPY_WIF.name())) {
             StringSelection sS = new StringSelection(generatedKeys.privateKey);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(sS, null);
-        } else if (e.getActionCommand().equals(Actions.COPYBTC.name())) {
+        } else if (e.getActionCommand().equals(Actions.COPY_BTC.name())) {
             StringSelection sS = new StringSelection(generatedKeys.bitcoinAddress);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(sS, null);
+        } else if (e.getActionCommand().equals(Actions.PASTE_SEED.name())) {
+            String hexSeed = getClipboardContents();
+            if ((hexSeed != null) && (hexSeed.length() == 64) && (hexSeed.matches("-?[0-9a-fA-F]+"))) {
+                generatedKeys = new GenerateKeys(hexSeed.toUpperCase());
+                updateLabels();
+            }
+        } else if (e.getActionCommand().equals(Actions.SHOW_QR.name())) {
+            // generate new popup window with the Public Key QR-CODE
+            JFrame qrFrame = new JFrame("Public Key QR code");
+            qrFrame.getContentPane().setLayout(new FlowLayout());
+            try {
+                qrFrame.getContentPane().add(new JLabel(new ImageIcon(createQRImage(generatedKeys.bitcoinAddress, 350))));
+            } catch (WriterException ex) {
+                throw new RuntimeException(ex);
+            }
+            qrFrame.pack();
+            qrFrame.setLocation(800, 100);
+            qrFrame.setVisible(true);
+        } else if (e.getActionCommand().equals(Actions.WEBCAM.name())) {
+            // get VGA picture from webcam
+            Webcam webcam2 = Webcam.getDefault();
+            webcam2.setViewSize(new Dimension(640, 480));
+            webcam2.open();
+            BufferedImage image = webcam2.getImage();
+            webcam2.close();
+            // save to disk with random name
+            // get the SHA-256 hash from that file
+            SecureRandom sr = new SecureRandom();
+            String name = "webcam_" + sr.nextInt(0, 2147483647) + ".png";
+            String shaString;
+            try {
+                ImageIO.write(image, "PNG", new File(name));
+                // the file is small at about 300-700kB so we read-in the complete file in memory
+                byte[] data = Files.readAllBytes(Paths.get(name));
+                byte[] hash = MessageDigest.getInstance("SHA-256").digest(data);
+                shaString = byteArrayToHexString(hash);
+            } catch (IOException | NoSuchAlgorithmException ex) {
+                throw new RuntimeException(ex);
+            }
+            // generate the bitcoin key using the calculated 32 byte SHA-256 hash as the private key
+            generatedKeys = new GenerateKeys(shaString);
+            updateLabels();
+            // show the picture
+            JFrame webcamFrame = new JFrame("WebCAM picture");
+            webcamFrame.getContentPane().setLayout(new FlowLayout());
+            webcamFrame.getContentPane().add(new JLabel(new ImageIcon(image)));
+            webcamFrame.pack();
+            webcamFrame.setLocation(100, 250);
+            webcamFrame.setVisible(true);
         }
     }
 
@@ -132,66 +207,69 @@ public class Main implements Printable, ActionListener {
     }
 
     public int print(Graphics g, PageFormat pf, int page) {
-        if (page > 1) {
-            return NO_SUCH_PAGE;
-        }
-        if (page == 0) {
-            Graphics2D g2d = (Graphics2D) g;
-            g.setFont(new Font("Monospaced", Font.PLAIN, 8));
-            g2d.translate(pf.getImageableX(), pf.getImageableY());
-            BufferedImage QRimageWIF = null, QRimageBitcoinAddress = null, RandomImage1 = null, RandomImage2 = null;
-            try {
-                QRimageWIF = createQRImage(generatedKeys.privateKey, 120);
-                QRimageBitcoinAddress = createQRImage(generatedKeys.bitcoinAddress, 120);
-                RandomImage1 = createRandomImage(125, 150);
-                RandomImage2 = createRandomImage(100);
-            } catch (WriterException e) {
-                e.printStackTrace();
+        switch (page) {
+            case 0 -> {
+                Graphics2D g2d = (Graphics2D) g;
+                g.setFont(new Font("Monospaced", Font.PLAIN, 8));
+                g2d.translate(pf.getImageableX(), pf.getImageableY());
+                BufferedImage QRimageWIF = null, QRimageBitcoinAddress = null, RandomImage1 = null, RandomImage2 = null;
+                try {
+                    QRimageWIF = createQRImage(generatedKeys.privateKey, 120);
+                    QRimageBitcoinAddress = createQRImage(generatedKeys.bitcoinAddress, 120);
+                    RandomImage1 = createRandomImage(125, 150);
+                    RandomImage2 = createRandomImage(100);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                g.drawImage(QRimageBitcoinAddress, 20, 50, 110, 110, null);
+                g.drawString("PUBLIC ADDRESS", 40, 150);
+                g.drawString(generatedKeys.bitcoinAddress, 5, 45);
+                g.drawString(generatedKeys.bitcoinAddress, 5, 165);
+                g.drawImage(RandomImage1, 190, 30, 125, 150, null);
+                g.drawImage(RandomImage2, 355, 55, 100, 100, null);
+                g.drawImage(QRimageWIF, 460, 45, 120, 120, null);
+                g.drawString(generatedKeys.privateKey, 322, 50);
+                g.drawString(generatedKeys.privateKey, 322, 165);
+                g.drawLine(0, 10, 325, 10);
+                g.drawLine(325, 10, 335, 30);
+                g.drawLine(335, 30, 570, 30);
+                g.drawLine(0, 200, 325, 200);
+                g.drawLine(325, 200, 335, 180);
+                g.drawLine(335, 180, 570, 180);
+                g.drawLine(340, 60, 340, 145);
+                g.drawLine(185, 20, 185, 190);
+                return PAGE_EXISTS;
             }
-            g.drawImage(QRimageBitcoinAddress, 20, 50, 110, 110, null);
-            g.drawString("PUBLIC ADDRESS", 40, 150);
-            g.drawString(generatedKeys.bitcoinAddress, 5, 45);
-            g.drawString(generatedKeys.bitcoinAddress, 5, 165);
-            g.drawImage(RandomImage1, 190, 30, 125, 150, null);
-            g.drawImage(RandomImage2, 355, 55, 100, 100, null);
-            g.drawImage(QRimageWIF, 460, 45, 120, 120, null);
-            g.drawString(generatedKeys.privateKey, 322, 50);
-            g.drawString(generatedKeys.privateKey, 322, 165);
-            g.drawLine(0, 10, 325, 10);
-            g.drawLine(325, 10, 335, 30);
-            g.drawLine(335, 30, 570, 30);
-            g.drawLine(0, 200, 325, 200);
-            g.drawLine(325, 200, 335, 180);
-            g.drawLine(335, 180, 570, 180);
-            g.drawLine(340, 60, 340, 145);
-            g.drawLine(185, 20, 185, 190);
-        }
-        if (page == 1) {
-            Graphics2D g2d = (Graphics2D) g;
-            g.setFont(new Font("Monospaced", Font.PLAIN, 8));
-            g2d.translate(pf.getImageableX(), pf.getImageableY());
-            BufferedImage RandomRect = null;
-            try {
-                RandomRect = createRandomImage(120, 20);
-            } catch (WriterException e) {
-                e.printStackTrace();
+            case 1 -> {
+                Graphics2D g2d = (Graphics2D) g;
+                g.setFont(new Font("Monospaced", Font.PLAIN, 8));
+                g2d.translate(pf.getImageableX(), pf.getImageableY());
+                BufferedImage RandomRect = null;
+                try {
+                    RandomRect = createRandomImage(120, 20);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                g.drawLine(260, 10, 570, 10);
+                g.drawLine(260, 200, 570, 200);
+                g.drawLine(260, 10, 260, 200);
+                g.drawLine(570, 10, 570, 200);
+                g.drawLine(300, 80, 530, 80);
+                g.drawLine(300, 130, 530, 130);
+                g.drawLine(300, 180, 530, 180);
+                g.drawString("Private key (WIF)", 140, 80);
+                g.drawString("inside here", 150, 100);
+                g.setFont(new Font("Monospaced", Font.BOLD, 10));
+                g.drawString("KEEP HIDDEN", 140, 140);
+                g.drawString("BITCOIN PAPER WALLET (SEGWIT)", 325, 30);
+                g.drawImage(RandomRect, 120, 40, 120, 20, null);
+                g.drawImage(RandomRect, 120, 155, 120, 20, null);
+                return PAGE_EXISTS;
             }
-            g.drawLine(260, 10, 570, 10);
-            g.drawLine(260, 200, 570, 200);
-            g.drawLine(260, 10, 260, 200);
-            g.drawLine(570, 10, 570, 200);
-            g.drawLine(300, 80, 530, 80);
-            g.drawLine(300, 130, 530, 130);
-            g.drawLine(300, 180, 530, 180);
-            g.drawString("Private key (WIF)", 140, 80);
-            g.drawString("inside here", 150, 100);
-            g.setFont(new Font("Monospaced", Font.BOLD, 10));
-            g.drawString("KEEP HIDDEN", 140, 140);
-            g.drawString("BITCOIN PAPER WALLET (SEGWIT)", 325, 30);
-            g.drawImage(RandomRect, 120, 40, 120, 20, null);
-            g.drawImage(RandomRect, 120, 155, 120, 20, null);
+            default -> {
+                return NO_SUCH_PAGE;
+            }
         }
-        return PAGE_EXISTS;
     }
 
     private static BufferedImage createQRImage(String qrCodeText, int size) throws WriterException {
@@ -217,21 +295,7 @@ public class Main implements Printable, ActionListener {
     }
 
     private static BufferedImage createRandomImage(int size) throws WriterException {
-        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        image.createGraphics();
-        Graphics2D graphics = (Graphics2D) image.getGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, size, size);
-        graphics.setColor(Color.BLACK);
-        SecureRandom sr1 = new SecureRandom();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (sr1.nextBoolean()) {
-                    graphics.fillRect(i, j, 1, 1);
-                }
-            }
-        }
-        return image;
+        return createRandomImage(size, size);
     }
 
     private static BufferedImage createRandomImage(int size1, int size2) throws WriterException {
@@ -254,16 +318,33 @@ public class Main implements Printable, ActionListener {
 
     public static class GenerateKeys {
         public String hexRandom, privateKey, bitcoinAddress;
+        private final static String UPPER_LIMIT = "F".repeat(56);  // safe upper limit for validity of ECDSA
 
         public GenerateKeys() {
-            final String UPPER_LIMIT = "F".repeat(56);  // safe upper limit for validity of ECDSA
             byte[] random32bytes = new byte[32];
             SecureRandom sr = new SecureRandom();  // using cryptographic safe random function
+            String hexTest;
             do {
                 sr.nextBytes(random32bytes);
-                hexRandom = byteArrayToHexString(random32bytes);
+                hexTest = byteArrayToHexString(random32bytes);
             }
-            while (hexRandom.substring(0, 55).equals(UPPER_LIMIT));
+            while (hexTest.substring(0, 55).equals(UPPER_LIMIT));
+            hexRandom = hexTest;
+            CalcKey();
+        }
+
+        public GenerateKeys(String hexRandomString) {
+            if (hexRandomString.length() == 64) {
+                hexRandom = hexRandomString;
+            } else {
+                throw new IllegalArgumentException("Given string must be 32 bytes long, but is " + hexRandomString.length() / 2 + " long");
+            }
+            if (hexRandomString.substring(0, 55).equals(UPPER_LIMIT))
+                throw new IllegalArgumentException("Given string is outside of valid ECDSA allowed range");
+            CalcKey();
+        }
+
+        private void CalcKey() {
             privateKey = Base58CheckEncode("80" + hexRandom + "01"); // private key in WIF format
             String compressedPubKey = privToCompressedPublic(hexRandom);
             String rawCompressedBitcoinAddress = hashShaRipemd(compressedPubKey);
@@ -272,6 +353,7 @@ public class Main implements Printable, ActionListener {
             bitcoinAddress = Base58CheckEncode("05" + hashedRedeemScript); // public bitcoin address, Segwit style
             logger.info(bitcoinAddress);
         }
+
     }
 
     private static String Base58CheckEncode(String address) {
@@ -327,6 +409,21 @@ public class Main implements Printable, ActionListener {
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
         return new String(hexChars);
+    }
+
+    private String getClipboardContents() {
+        String result = "";
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable contents = clipboard.getContents(null);
+        boolean hasTransferableText = (contents != null) && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+        if (hasTransferableText) {
+            try {
+                result = (String) contents.getTransferData(DataFlavor.stringFlavor);
+            } catch (UnsupportedFlavorException | IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result;
     }
 
 }
